7bac08279b45854dcec9553887a838bd0a93a275
biernate <biernatevan33@gmail.com>
2025-09-03 17:15:22 -0400

    Uploading Hello Git program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Evan Biernat
+ *        Date: 9/3/2025
+ * Description: Says Hello Git!
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
